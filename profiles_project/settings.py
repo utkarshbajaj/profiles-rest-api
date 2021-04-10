d9a0fa4e3f8ccfa672010f16e10f338987c349a0
Utkarsh Bajaj <44445191+utkarshbajaj@users.noreply.github.com>
2021-04-10 20:29:36 +0530

    adding deploy on aws

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'qp#=-k4(yb029&64h79=u6q$gcbr6)stc^x_x&0^a**y_$fas&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+STATIC_ROOT = 'static/'
